e248f77da365f2e6e1d2b1fc1c68ded08e48d7ff
okoJanniy <82619523+okoJanniy@users.noreply.github.com>
2021-04-28 02:08:46 +0300

    Reworked the dropdown test

--- src/test/java/DropDownTest.java
+++ src/test/java/DropDownTest.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class DropDownTest extends BaseTest {
 
@@ -10,10 +13,13 @@ public class DropDownTest extends BaseTest {
         driver.get("http://the-internet.herokuapp.com/dropdown");
         WebElement dropDown = driver.findElement(By.id("dropdown"));
         Select select = new Select(dropDown);
-        select.selectByVisibleText("Option 1");
-        select.selectByValue("1");
         select.selectByVisibleText("Option 2");
-        select.selectByValue("2");
-
+        String selectedText = select.getFirstSelectedOption().getText();
+        Assert.assertEquals(selectedText, "Option 2");
+        List<WebElement> options = select.getOptions();
+        for (WebElement element : options) {
+            System.out.println(element.getText());
+        }
+        driver.quit();
     }
 }
