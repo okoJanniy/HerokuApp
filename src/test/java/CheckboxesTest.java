09b690f4a8502c2ab680fcfd364eadb74e120633
okoJanniy <82619523+okoJanniy@users.noreply.github.com>
2021-04-28 00:19:00 +0300

    Created a checkBoxes test

---
+++ src/test/java/CheckboxesTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class CheckboxesTest extends BaseTest {
+
+    @Test
+    public void CheckAndPushCheckBoxes() {
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
+        WebElement checkBox1 = driver.findElement(By.cssSelector("[type=checkbox]"));
+        if (checkBox1.isSelected()) {
+            System.out.println("Флажок установлен");
+        } else {
+            System.out.println("Флажок отключен");
+            checkBox1.click();
+        }
+        if (checkBox1.isSelected()) {
+            System.out.println("Флажок установлен");
+        }
+        WebElement checkBox2 = driver.findElement(By.cssSelector("[type=checkbox]"));
+        if (checkBox2.isSelected()) {
+            System.out.println("Флажок установлен");
+        }else{
+            System.out.println("Флажок отключен");
+            checkBox2.click();
+        }
+        if (checkBox2.isSelected()){
+            System.out.println("Флажок отключен");
+        }
+    }
+}
