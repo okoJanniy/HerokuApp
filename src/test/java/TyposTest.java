fd3004fc28638fae7ad5b8a0d3231ac15419c09d
okoJanniy <82619523+okoJanniy@users.noreply.github.com>
2021-04-29 20:55:09 +0300

    Rename text1 - text2

--- src/test/java/TyposTest.java
+++ src/test/java/TyposTest.java
@@ -12,8 +12,8 @@ public class TyposTest extends BaseTest {
     public void CheckParagraphSpelling() {
         driver.get("http://the-internet.herokuapp.com/typos");
         List<WebElement> typos = driver.findElements(By.tagName("p"));
-        String text1 = typos.get(1).getText();
-        assertEquals(text1, "Sometimes you'll see a typo, other times you won't.",
+        String text2 = typos.get(1).getText();
+        assertEquals(text2, "Sometimes you'll see a typo, other times you won't.",
                 "Texts do not match");
 
     }
